2b1c39b8607137ad54182a9fb494913bff5b0fb7
Melisa Terstena <melisaterstenaa@gmail.com>
2025-05-16 16:41:33 +0200

    mm

--- functions.php
+++ functions.php
@@ -1,7 +1,45 @@
 <?php
-function load_script(){
-    wp_enqueue_style('tech_style',get_stylesheet_uri(),array(),filemtime(get_template_directory(). '/style.css'),'all');
 
+
+function load_scripts(){
+    wp_enqueue_style(
+        'style', 
+        get_stylesheet_uri(),
+        array(), 
+        filemtime(get_template_directory() . '/style.css'),
+        'all'
+    );
+
+
+    wp_enqueue_script('dropdown',get_template_directory_uri().'/js/dropdown.js',array(),'1.0',false);
 }
-add_action('wp_enqueue_scripts','load_script');
+add_action('wp_enqueue_scripts', 'load_scripts');
+
+function config(){
+    register_nav_menus(
+        array(
+            'wp_devs_main_menu'=>'Main Menu',
+             'wp_devs_footer_menu'=>'Footer Menu'
+        )
+        );
+
+        $args=array(
+            'height'=>225,
+            'width'=>1920
+        );
+
+        add_theme_support('custum-header',$args);
+         add_theme_support('post-thumbnails');
+          add_theme_support('custum-logo',array(
+             'height'=>110,
+            'width'=>200,
+            'flex-height'=>true,
+            'flex-width'=>true
+          ));
+          add_theme_support('automatic-feed-links');
+          add_theme_support('html5',array('comment-lists','comment-form','search-form','gallery','caption','style','script'));
+          add_theme_support('title-tag');
+}
+add_action('after_setup_theme','config',0);
+
 ?>
