b1f7a0053431c250d18e5718ac9fe43559e0ae6b
Vlere Mustafa <vlere.mustafa@digitalschool.tech>
2025-05-16 16:42:10 +0200

    lesson2

--- index.php
+++ index.php
@@ -3,7 +3,42 @@
 get_header();
 ?>
 
-    <h1>My theme</h1>
+    <div id="content" class="site-content">
+        <div id="primary" class="content-area">
+            <main id="main" class="site-main">
+                <h1>Blog</h1>
+                <div class="container">
+                    <div class="blog-items">
+                        <?php
+                            if(have_posts()):
+                                while(have_posts()): the_post();
+                                // get_template_part('parts/content');
+                                endwhile;
+                        ?>
+                            <div class="wpdevs-pagination">
+                                <div class="pages new">
+                                    <?php previous_posts_link("<<Newer posts");
+                                    ?>
+                                </div>
+                                <div class="pages old">
+                                        <?php next_posts_link("older posts>>");
+                                        ?>
+
+                                </div>
+
+                            </div>
+                            <?php
+                            else:
+                            ?>
+                            <p>Nothing yet to be displayed!</p>
+                            <?php endif;?>
+                    </div>
+                </div>
+            </main>
+
+        </div>
+
+    </div>
 
 <?php get_footer();?>
 
