8d7f2fd278c7c5070b0e7125ceeeec22954fe07a
ensar mustafa <ensarmustafa110@gmail.com>
2025-05-23 16:39:53 +0200

    ..

--- footer.php
+++ footer.php
@@ -1,2 +1,18 @@
+<footer class="site-former">
+    <div class="container">
+        <div class="copyright">
+
+        </div>
+        <div>
+           <?php wp_nav_menu(array('theme_location'=>'wp_devs_footer_menu','depth'=>1));
+           ?>
+        </div>
+    </div>
+    <?php wp_footer();?>
+</footer>
+
+
+
+
 </body>
 </html>
