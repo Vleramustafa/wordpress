4c83a953737f5c091bf87fe65a206562f6a6fbaf
Atik Mexhuani <atikmexhuani69.com>
2025-05-23 16:37:08 +0200

    aias

--- footer.php
+++ footer.php
@@ -1,4 +1,16 @@
+<footer class="site-footer">
+    <div class="container">
+        <div class="copyrigh">
 
-    
+        </div>
+        <div class="footer-menu">
+            <?php
+            wp_nav_menu(array('theme_location'=>'wp_devs_footer_menu','depth'=>1))
+            ?>
+        </div>
+    </div>
+    <?php wp_footer();?>
+</footer>
+
 </body>
 </html>
--- functions.php
+++ functions.php
@@ -1,6 +1,54 @@
 <?php
-function load_script(){
-    wp_enqueue_style('tech_style',get_stylesheet_uri(),array(),filemtime(get_template_directory(). '/style.css'),'all');
+ function load_scripts(){
+    wp_enqueue_style(
+        'style', 
+        get_stylesheet_uri(),
+        array(), 
+        filemtime(get_template_directory() . '/style.css'),
+        'all'
+    );
+
+
+    wp_endque_style('bootstrap',get_template_directory_uri().'/css/bootstrap.min.css');
+
+
+
+    wp_enqueue_script('dropdown',get_template_directory_uri().'/js/dropdown.js',array(),'1.0',false);
+
+    wp_enqeue_script('bootstrap2',get_template_directory_uri() .'/js/bootstrap.budle.min.js',array('jquery'));
 }
-add_action('wp_enqueue_scripts','load_script');
+add_action('wp_enqueue_scripts', 'load_scripts');
+function config(){
+    register_nav_menus(
+        array(
+            'wp_devs_main_menu'=>'Main Menu',
+            'wp_devs_footer_menu'=>'Footer Menu'
+        )
+        );
+    
+        $args=array(
+            'height'=>225,
+            'width'=>1920
+        );
+        add_theme_support('custom-header',$args);
+        add_theme_support('post-thumbnails');
+        add_theme_support('custom-logo',array(
+            'height'=>110,
+            'width'=>200,
+            'flex-height'=>true,
+            'flex-width'=>true
+
+
+        ));
+
+
+        add_theme_support('automatic-feed-links');
+        add_theme_support('html5',array('comment-list','comment-form','search-form','gallery','caption','style','script'));
+        add_theme_support('title-tag');
+
+
+
+}
+add_action('after_setup_theme','config',0);
+
 ?>
