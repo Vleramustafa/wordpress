a6ed587c21aa85dd0afb3532edeedd42857bc977
Vlere Mustafa <vlere.mustafa@digitalschool.tech>
2025-06-13 16:41:54 +0200

    lesson8

--- parts/content-search.php
+++ parts/content-search.php
@@ -5,8 +5,18 @@
             <div class="meta-info">
                 <p>Posted in <?php echo get_the_date();?> by <?php the_author_posts_link();?></p>
                 <?php if(has_category()):?>
-                    
 
+
+                    <p> Categories:<?php the_category( ' ' );?></p>
+                <?php endif;?>
+
+                <?php if(has_tag()):?>
+                    <p>Tags: <?php the_tags( '',', ');?></p>
+                <?php endif ?>
+
+            </div>
+            <div class="content">
+            <?php the_excerpt();?>
             </div>
     </header>
 
