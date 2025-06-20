7e14e267f555a84366ac9bf181707d24bd463347
Artur Halili <haliliartur894@gmail.com>
2025-06-20 16:40:12 +0200

    p

--- footer.php
+++ footer.php
@@ -1,2 +1,18 @@
+<footer class="site-footer">
+    <div class="container">
+        <div class="copyright">
+<p><?php echo get_theme_mod('set_copyright','Copyright X-All Rights Reserved');?></p>
+
+        </div>
+        <div class="footer-menu">
+            <?php
+                wp_nav_menu(array('theme_location'=>'wp_devs_footer_menu','depth'=>1));
+            ?>
+        </div>
+    </div>
+    <?php wp_footer();?>
+</footer>
+
+
 </body>
 </html>
--- header.php
+++ header.php
@@ -5,7 +5,56 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <?php
-    wp_head();
+        wp_head();
     ?>
-</head>
-<body>
+     </head>
+    <body>
+        <?php body_class();?>
+        <?php wp_body_open();?>
+        <div id="page" class="site">
+        <header>
+            <section class="top-bar">
+                <div class="container">
+                    <div class="logo">
+                    <?php 
+                        if(has_custom_logo(  )){
+                            the_custom_logo(  );
+                        }else{
+                            ?>
+                            <a href="<?php echo home_url('/') ?>"><span><?php bloginfo('name');?></span></a>
+                            <?php
+                        }
+                    ?>
+                    </div>
+                    <div class="searchbox">
+                        <?php get_search_form();?>
+
+                </div>
+            </section>
+                    
+        </div>
+                        
+    <?php
+        if(! is_page('landing_page')):?>
+        <section class="menu-area">
+            <div class="container">
+                <nav class="main-menu">
+                    <button class="check-button"> 
+                        <div class="menu-icon">
+                            <div class="bar1"></div>
+                            <div class="bar2"></div>
+                            <div class="bar3"></div>
+                        </div>
+                    </button>
+                    <?php
+                    wp_nav_menu(array('theme_location'=>'wp_devs_main_menu','depth'=>2));
+                    ?>
+                </nav>
+
+
+            </div>
+
+
+        </section>
+    <?php endif;?>
+   
