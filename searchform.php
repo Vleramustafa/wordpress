8c51c85edcba1f84f14869a7a487fbbb9229ee1d
Melisa Terstena <melisaterstenaa@gmail.com>
2025-06-27 16:30:15 +0200

    jj

---
+++ searchform.php
@@ -0,0 +1,7 @@
+<form role="search" method="get" id="searchform" class="searchform" action="<?php echo esc_url(home_url('/'))?>">
+    <div>
+        <label for="screen-reader-text" for="s"> </label>
+            <input type="text" value="<?php echo get_search_query();?>" name="s" id="s">
+            <input type="submit" id="searchsubmit" value="Search">
+    </div>
+</form>
