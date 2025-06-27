8b3ae0c280268cc0ef64bc77dd2896086d3bf61a
Atik Mexhuani <atikmexhuani69@gmail.com>
2025-06-27 16:29:52 +0200

    aaa

--- parts/content-search.php
+++ parts/content-search.php
@@ -1,21 +1,28 @@
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<article id="post-<?php the_ID();?>" <?php post_class();?>>
     <header>
-        <h2>
-            <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-            <?php if (has_post_type()):?>
-                <div class="meta-info">
-                    <p>Posted in <?php echo get_the_date();?> by <?php the_author_posts_link();?></p>
-                    <?php if (has_category()): ?>
-                        <p>Categories: <?php the_category(' '); ?></p>  
-                    <?php endif; ?>
-                    <?php if (has_tag()): ?>
-                        <p>Tags: <?php the_tags('', ', '); ?></p>
-                    <?php endif; ?>                        
+        <h2><a href="<?php the_permalink();?>"><?php the_title();?></h2>
+        <?php if('post'==get_post_type()):?>
+            <div class="meta-info">
+                <p>Posted in <?php echo get_the_date();?> by <?php the_author_posts_link();?></p>
+                <?php if(has_category()):?>
 
-                </div> 
-                <div class="content">
-                    <?php the_excerpt(); ?>
+
+
+                    <p> Categories:<?php the_category( ' ' );?></p>
+                
+                <?php endif;?>
+
+
+                <?php if(has_tag()):?>
+                    <p>Tags: <?php the_tags( '',', ');?></p>
+                <?php endif ?>
                 </div>
-    </header>
+                <?php endif;?>
+           </header>
+            <div class="content">
+            <?php the_excerpt();?>
+            </div>
+    
+
 
 </article>
