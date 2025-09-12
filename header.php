9d5c24cddcba0dd3c5f723ede702280efb95875f
Atik Mexhuani <atikmexhuani69@gmail.com>
2025-09-12 16:48:27 +0200

    gyigvfyucv

--- header.php
+++ header.php
@@ -4,19 +4,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <?php
+     <?php
         wp_head();
     ?>
-    </head>
-<body>
+     </head>
+    <body>
 
-    <?php wp_body_open();?>
-    <div id="page" class="site">
-        <header>
-            <section class="top-bar">
-                <div class="container">
-                    <div class="logo">
-  <?php 
+
+   <?php wp_body_open();?>
+   <div id="page" class="site">
+    <header> 
+        <section class="top-bar">
+            <div class="container">
+                <div class="logo">
+                    <?php 
                         if(has_custom_logo(  )){
                             the_custom_logo(  );
                         }else{
@@ -25,47 +26,16 @@
                             <?php
                         }
                     ?>
-                    </div>
                 </div>
                 <div class="searchbox">
                     <?php get_search_form();?>
 
-                </div>
-            </section>
-    </div>
-    <?php
-    if(! is_page('landing_page')):?>
-    <section class="menu-area">
-        <div class="container">
-            <nav class="main-menu">
-                <button class="check-button">
-                    <div class="menu-icon">
-                        <div class="bar1"></div>
-                        <div class="bar2"></div>
-                        <div class="bar3"></div>
-                    </div>
-                </button>
-                <?php
-                wp_nav_menu(array('theme_location'=>'wp_devs_main_menu','depth'=>2))
-                ?>
-            </nav>
-        </div>
 
-    </section>
-    <?php endif;?>
-    <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-     </head>
-    <body>
+                </div>
+            </div>
+        </section>
     <?php
-        wp_head();
-    ?>
-    <?php
-        if(! is_page('landing_page')):?>
+         if(! is_page('landing_page')):?>
         <section class="menu-area">
             <div class="container">
                 <nav class="main-menu">
@@ -87,4 +57,7 @@
 
         </section>
     <?php endif;?>
+
+
+    </header>
    
