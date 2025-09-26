03a27c793ebb3f234ca2664f526cce62a5d700e3
Vlere Mustafa <vlera.mustafa.11@gmail.com>
2025-09-26 16:45:56 +0200

    lesson14

---
+++ page-about.php
@@ -0,0 +1,41 @@
+<?php get_header();?>
+
+<div id="content" class="site-content">
+    <main id="main" class="site-main">
+        <section class="about-hero">
+            <div class="container">
+                <h1>
+                    About Us
+                </h1>
+                <p>Welcome to out tech corner-where innovation meets explanation.</p>
+            </div>
+        </section>
+        <section class="about-info">
+            <div class="container">
+                <h2>Who We Are</h2>
+                <p>We are a team of developers,engineers and tech who loves to write code.</p>
+            </div>
+        </section>
+
+        <section class="about-what-we-cover">
+            <div class="container">
+                <h2>What We Cover</h2>
+                <ul>
+                    <li>Programming Tutorials and tips</li>
+                    <li>Programming Tutorials and tips</li>
+                    <li>Programming Tutorials and tips</li>
+                    <li>Programming Tutorials and tips</li>
+                    <li>Programming Tutorials and tips</li>
+                </ul>
+            </div>
+        </section>
+
+        <section class="about-contact">
+            <div class="container">
+                <h2>Want to Connect?</h2>
+                <p>We love hearing from our readers!Reach out to us via our Contact Page</p>
+            </div>
+        </section>
+    </main>
+</div>
+<?php get_footer();?>
